fbce569e82f4c01d11c2fb9969c693f920cdd718
Sam <sam.camhi@gmail.com>
2023-03-19 18:00:16 -0400

    Passed Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
 
-// Write your code here
+const num2 = 2;
+
+const num3 = 40;
+
+const num4 = 6;
+
+const set = (5, 10, 15, 20);
+
+const multiply = num1 * num2;
+
+const random = (Math.floor(Math.random() * 10) + 1);
+
+const mod = num3 % num4;
+
+const max = Math.max(set);
